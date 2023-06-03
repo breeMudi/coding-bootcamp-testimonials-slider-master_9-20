2cc92fb7fc5586c1d1631eb167a7bf661ceec24a
Bridget Mudiaga-Ojemu <65234249+breeMudi@users.noreply.github.com>
2023-06-04 01:54:49 +0200

    Update main.js
    final js

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const curr = '.tanya'
+// const curr = '.tanya'
 
-function next(){
-    // name.style.left = 1500
-    const tanya = document.querySelector(`${curr}`)
-    const john = document.querySelector('.john')
+function next(now){
+    const tanya = document.querySelector(`.${now[0]}`)
+    const john = document.querySelector(`.${now[1]}`)
     tanya.style.left = '1500px'
     john.style.left = '0px'
 }
 
-function prev(){
-    // name.style.left = 1500
-    const tanya = document.querySelector(`${curr}`)
-    const john = document.querySelector('.john')
-    tanya.style.left = '0px'
-    john.style.left = '1500px'
+function prev(now){
+    const tanya = document.querySelector(`.${now[0]}`)
+    const john = document.querySelector(`.${now[1]}`)
+    tanya.style.left = '1500px'
+    john.style.left = '0px'
 }
